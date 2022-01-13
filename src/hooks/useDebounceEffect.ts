f3199838d4195a6ccc9486ec3e1ff89039cc497f
harmaus <harmaus@defray.com>
2022-01-13 18:09:40 +0200

    feat(hooks): add useDebounceEffect hook

---
+++ src/hooks/useDebounceEffect.ts
@@ -0,0 +1,32 @@
+import { DependencyList, EffectCallback, useEffect, useRef } from 'react';
+
+export function useDebouncedEffect(
+  effect: EffectCallback,
+  delay: number,
+  deps: DependencyList,
+) {
+  const data = useRef<{ firstTime: boolean; clearFunc?: void | unknown }>({
+    firstTime: true,
+  });
+  useEffect(() => {
+    const { firstTime, clearFunc } = data.current;
+
+    if (firstTime) {
+      data.current.firstTime = false;
+      return;
+    }
+
+    const handler = setTimeout(() => {
+      if (clearFunc && typeof clearFunc === 'function') {
+        clearFunc();
+      }
+      data.current.clearFunc = effect();
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+    // we dont need effect to be as dependency.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [delay, ...deps]);
+}
