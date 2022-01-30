621ec4f4670b2ce4b9e6211cd9aad06c9fdcb85f
harmaus <harmaus@defray.com>
2022-01-30 12:16:12 +0200

    feat(wallet): disable tx submitting for read-only wallets

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -8,6 +8,7 @@ export const contracts = checkContracts([
   {
     name: 'sovrynProtocol',
     address: {
+      30: '0x5A0D867e0D70Fcc6Ade25C3F1B89d618b5B4Eaa7',
       31: '0x25380305f223B32FDB844152abD2E82BC5Ad99c3',
     },
   },
@@ -21,24 +22,21 @@ export const contracts = checkContracts([
   {
     name: 'priceFeeds',
     address: {
+      30: '0x437AC62769f386b2d238409B7f0a7596d36506e4',
       31: '0x7f38c422b99075f63C9c919ECD200DF8d2Cf5BD4',
-    },
-  },
-  {
-    name: 'protocolSettings',
-    address: {
-      31: '0xa29CCeD858D46a0F91CC48646fa849D47eC96090',
     },
   },
   {
     name: 'rbtcWrapper',
     address: {
+      30: '0xa917BF723433d020a15629eba71f6C2a6B38e52d',
       31: '0x6b1a4735b1e25cce9406b2d5d7417ce53d1cf90e',
     },
   },
   {
     name: 'swapNetwork',
     address: {
+      30: '0x98aCE08D2b759a265ae326F010496bcD63C15afc',
       31: '0x61172B53423E205a399640e5283e51FE60EC2256',
     },
   },
