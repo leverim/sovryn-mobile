e9768cc22aa104f3a4a0efeeb7b46d837d75d5ff
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2024-02-09 23:08:45 +0200

    fix: stuff

--- src/controllers/PassCodeController.ts
+++ src/controllers/PassCodeController.ts
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ethers } from 'ethers';
 import { Platform } from 'react-native';
 import EncryptedStorage from 'react-native-encrypted-storage';
